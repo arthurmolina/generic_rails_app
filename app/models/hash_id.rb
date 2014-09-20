7b4950dc307dfc88417e6766fc741916436b0aa9
vagrant <arthurmolina@gmail.com>
2014-09-20 19:48:48 +0000

    Using hashids to get unique hashs in model

---
+++ app/models/hash_id.rb
@@ -0,0 +1,13 @@
+# Abstract Class to use encrypted parameters
+class HashId < ActiveRecord::Base
+  self.abstract_class = true
+  
+  def to_param
+    Hashids.new( self.class.name, 8 ).encrypt( id )
+  end
+
+  def self.find_by_param( input )
+    find( Hashids.new( self.name, 8 ).decrypt( input ) ).first
+  end
+
+end
